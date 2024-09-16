0b331fe03cbf2f5a78e124fa5c9f65e14163f396
PaulinaD-code <paulina.duda.web.dev@gmail.com>
2024-09-16 16:08:25 +0200

    Update septemberMovies24.js

--- scripts/data/dataMoviesJS/septemberMovies24.js
+++ scripts/data/dataMoviesJS/septemberMovies24.js
@@ -2,7 +2,7 @@ export const septemberMovies = [
   {
     id: '01',
     anchorLink: './mediaOffer1.html',
-    movieImg: '/september2024/hopesolo.jpg',
+    movieImg: 'september2024/hopesolo.jpg',
     premiere: 'September 3, 2024',
     contentType: {
       film: true,
@@ -18,7 +18,7 @@ export const septemberMovies = [
   {
     id: '02',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/september2024/perfectcouple.jpg',
+    movieImg: 'september2024/perfectcouple.jpg',
     premiere: 'September 5, 2024',
     contentType: {
       film: true,
@@ -34,7 +34,7 @@ export const septemberMovies = [
   {
     id: '03',
     anchorLink: './mediaOffer3.html',
-    movieImg: '/september2024/rebelridge.jpg',
+    movieImg: 'september2024/rebelridge.jpg',
     premiere: 'September 6, 2024',
     contentType: {
       film: false,
@@ -50,7 +50,7 @@ export const septemberMovies = [
   {
     id: '04',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/september2024/perapalace.jpg',
+    movieImg: 'september2024/perapalace.jpg',
     premiere: 'September 12, 2024',
     contentType: {
       film: true,
@@ -66,7 +66,7 @@ export const septemberMovies = [
   {
     id: '05',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/september2024/uglies.jpg',
+    movieImg: 'september2024/uglies.jpg',
     premiere: 'September 13, 2024',
     contentType: {
       film: false,
@@ -82,7 +82,7 @@ export const septemberMovies = [
   {
     id: '06',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/september2024/twilight.jpg',
+    movieImg: 'september2024/twilight.jpg',
     premiere: 'September 19, 2024',
     contentType: {
       film: false,
@@ -98,7 +98,7 @@ export const septemberMovies = [
   {
     id: '07',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/september2024/hisdaughters.jpg',
+    movieImg: 'september2024/hisdaughters.jpg',
     premiere: 'September 20, 2024',
     contentType: {
       film: false,
@@ -114,7 +114,7 @@ export const septemberMovies = [
   {
     id: '08',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/september2024/everything.jpg',
+    movieImg: 'september2024/everything.jpg',
     premiere: 'September 26, 2024',
     contentType: {
       film: true,
