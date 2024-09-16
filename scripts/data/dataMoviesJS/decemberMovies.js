6433fe9d7000e57058e7c5ab4a23c9e0f39a566f
PaulinaD-code <paulina.duda.web.dev@gmail.com>
2024-09-16 16:18:30 +0200

    Update decemberMovies.js

--- scripts/data/dataMoviesJS/decemberMovies.js
+++ scripts/data/dataMoviesJS/decemberMovies.js
@@ -2,7 +2,7 @@ export const decemberMovies = [
   {
     id: '1',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/december2024/thatchristmas.jpg',
+    movieImg: 'december2024/thatchristmas.jpg',
     premiere: 'December 6, 2024',
     contentType: {
       film: true,
@@ -18,7 +18,7 @@ export const decemberMovies = [
   {
     id: '2',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/december2024/carry-on.jpg',
+    movieImg: 'december2024/carry-on.jpg',
     premiere: 'December 13, 2024',
     contentType: {
       film: true,
@@ -34,7 +34,7 @@ export const decemberMovies = [
   {
     id: '3',
     anchorLink: './mediaOffer2r.html',
-    movieImg: '/december2024/AAAABQ-7cetDzOBP_4_cqMCOdAKXLN-qqcOaB46e_IUqrBfUERo6NVqzG0ouBMS43WgXCXla6wDE4Xw5mtBWCuv2vZCXhclRgYqAykrF-rPFEvyonQrHPNZYm_spolWQF6I3Gyjb1w.jpg',
+    movieImg: 'december2024/AAAABQ-7cetDzOBP_4_cqMCOdAKXLN-qqcOaB46e_IUqrBfUERo6NVqzG0ouBMS43WgXCXla6wDE4Xw5mtBWCuv2vZCXhclRgYqAykrF-rPFEvyonQrHPNZYm_spolWQF6I3Gyjb1w.jpg',
     premiere: 'December 20, 2024',
     contentType: {
       film: true,
