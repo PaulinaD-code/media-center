2a31672ac16784232daadda114ff705df2a19cea
Paulina <paulina.duda.web.dev@gmail.com>
2024-09-17 12:44:07 +0200

    Update changingButtons.js

--- scripts/data/changingButtons.js
+++ scripts/data/changingButtons.js
@@ -2,25 +2,25 @@ export const selectionButtons = [
   {
     id: 'film',
     name: 'Film',
-    icon: '/media/media--nav-btn/icons8-movie-24.png',
+    icon: '../media/media--nav-btn/icons8-movie-24.png',
     attribute: false
   },
   {
     id:'serial',
     name: 'Seriale',
-    icon: '/media/media--nav-btn/series.png',
+    icon: '../media/media--nav-btn/series.png',
     attribute: false
   },
   {
     id:'document',
     name: 'Dokument',
-    icon: '/media/media--nav-btn/document.png',
+    icon: '../media/media--nav-btn/document.png',
     attribute: false
   },
   {
     id:'forKids',
     name: 'Dla dzieci',
-    icon: '/media/media--nav-btn/group.png',
+    icon: '../media/media--nav-btn/group.png',
     attribute: false
   },
   {
@@ -32,19 +32,19 @@ export const selectionButtons = [
   {
     id:'realityTV',
     name: 'Reality TV',
-    icon: '/media/media--nav-btn/camera.png', 
+    icon: '../media/media--nav-btn/camera.png', 
     attribute: false
   },
   {
     id:'game',
     name: 'Gra',
-    icon: '/media/media--nav-btn/game.png',
+    icon: '../media/media--nav-btn/game.png',
     attribute: false
   },
   {
     id:'standUP',
     name: 'Stand-up',
-    icon: '/media/media--nav-btn/icons8-microphone-50.png',
+    icon: '../media/media--nav-btn/icons8-microphone-50.png',
     attribute: false
   },
 ]
